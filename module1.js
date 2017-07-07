e41b55b8f4e8c68d2ab78a53d1b26f9f3f04b236
svetlananikitina <nikitinasvetlana92@gmail.com>
2017-07-07 11:57:26 +0200

    bootstrap from cdn included

--- module1.js
+++ module1.js
@@ -83,19 +83,19 @@ function sendToBot (message) {
                     // res.send(body.msg)
                     document.getElementById("list-group").innerHTML = "Recieved answer from WitBot: "+body.msg;
 
-                    let messages = ['fisrt msg', 'second', 'third'];
-
-                    return messages.map(function(body) {
-                        let li = createNode('li'), //create elements we need to display
-                            span = createNode('span');
-                        let ul = document.getElementById('messages');
-
-                        console.log(body);
-
-                        span.innerHTML = "response is : "+ body;
-                        append (li, span);
-                        append (ul, li);
-                    })
+                    // let messages = ['fisrt msg', 'second', 'third'];
+                    //
+                    // return messages.map(function(body) {
+                    //     let li = createNode('li'), //create elements we need to display
+                    //         span = createNode('span');
+                    //     let ul = document.getElementById('messages');
+                    //
+                    //     console.log(body);
+                    //
+                    //     span.innerHTML = "response is : "+ body;
+                    //     append (li, span);
+                    //     append (ul, li);
+                    // })
                 }
 
                 if (body.type === 'action' && body.action === 'show_place') {
